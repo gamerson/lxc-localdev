567440b5bcd8fae582e077ef6f4860b5ef17e13b
Gregory Amerson <gregory.amerson@liferay.com>
2022-10-20 15:13:18 -0500

    add basic build command

--- scripts/ext/build.sh
+++ scripts/ext/build.sh
@@ -6,18 +6,23 @@ REPO="${LOCALDEV_REPO:-/repo}"
 
 # Check to see if tilt is up, if so tell that container to build, otherwise we can call build
 
-TILT_CONTAINER=$(docker container list --format '{{json .}}' | jq -sr '.[] | select(.Names=="localdev-extension-runtime")')
+TILT_CONTAINER=$(docker container list --format '{{json .}}' | jq -sr '.[] | select(.Names=="localdev-extension-runtime") | .Names')
 
-BUILD_CMD="/workspace/gradlew --project-dir /workspace clean build"
+if [ $# -eq 0 ]; then
+  BUILD_CMD='/workspace/gradlew --project-dir /workspace clean build'
+else
+  BUILD_CMD="/workspace/gradlew --project-dir /workspace $@"
+fi
 
 # call the build command at appropriate location
 
 if [ "$TILT_CONTAINER" != "" ]; then
-  BUILD_OUTPUT=$(docker exec -i $TILT_CONTAINER "sh -c \"$BUILD_CMD\"")
+  docker exec -i $TILT_CONTAINER sh -c "$BUILD_CMD"
 else
-  BUILD_OUTPUT=$($BUILD_CMD)
+  $BUILD_CMD
 fi
 
 # now we have built client extensions, search for the zips and then return in a string
-
-find /workspace/client-extensions -name 'dist/*.zip' 
+for distZip in $(find /workspace/client-extensions -name '*.zip' -path '*/dist/*' 2>/dev/null); do
+  echo $distZip | sed -e 's/\/workspace\/client-extensions\///'
+done
